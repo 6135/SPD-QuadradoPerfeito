858a45e78f77ad16811e0d4491d9d5db99cd05ec
6135 <guibscosta@hotmail.com>
2021-04-16 03:15:09 +0100

    asd

--- wsl_execute.sh
+++ wsl_execute.sh
@@ -2,10 +2,10 @@
 for filename in inputs/*.txt; do
     echo $filename
     for ((i=0;i<33;i++)); do
-        { time mpirun -np 2 perfectSquareMPI.out $filename 2>trash.txt ; } 2>> MPITests/$(basename "$filename" .txt)
+        { time mpirun -np 2 perfectSquareMPI.out $filename 2>trash.txt ; } 2>> MPITests/$(basename "$filename" )
     done
 
     for ((i=0;i<33;i++)); do
-        { time mpirun -np 2 perfectSquareHibrid.out $filename 2>trash.txt ; } 2>> HibridTests/$(basename "$filename" .txt)
+        { time mpirun -np 2 perfectSquareHibrid.out $filename 2>trash.txt ; } 2>> HibridTests/$(basename "$filename" )
     done
 done
